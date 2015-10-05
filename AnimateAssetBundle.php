e04b59a7aa87cffe82d4b0668592e278b585de09
Egor Pinkevich <egor.pinkevich@gmail.com>
2015-10-06 01:39:24 +0300

    'initial'

--- AnimateAssetBundle.php
+++ AnimateAssetBundle.php
@@ -1,1 +1,14 @@
 <?php
+
+namespace delocker\animate;
+
+use yii\web\AssetBundle;
+
+class AnimateAssetBundle extends AssetBundle
+{
+    public $sourcePath = '@vendor/delocker/yii2-animate.css';
+
+    public $css = [
+        'css/animate.min.css',
+    ];
+}
